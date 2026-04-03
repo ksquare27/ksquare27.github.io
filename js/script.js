dd29d10f795974b96a8053fce8f330f6dba1c58a
Kodjovi <273383042+ksquare27@users.noreply.github.com>
2026-04-03 12:45:15 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 async function fetchDailyReadings() {
   const res = await fetch('https://lectio-api.org/api/v1/readings/today?tradition=Roman Catholic');
   return await res.json();
+  async function fetchDailyReadings() {
+  const res = await fetch('https://lectio-api.org/api/v1/readings/today?tradition=Roman Catholic');
+  return await res.json();
+const enText = await fetchVerseText("John 20:1‑9", "en");
+const frText = await fetchVerseText("John 20:1‑9", "fr");
+const latText = await fetchVerseText("John 20:1‑9", "la");
 }
